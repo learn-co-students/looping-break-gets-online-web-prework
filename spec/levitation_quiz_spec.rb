81a9943613813a6f7b682a79cbb9d757cadc504b
David masterson <david73180@gmail.com>
2018-09-17 04:39:31 +0000

    Done.

--- spec/levitation_quiz_spec.rb
+++ spec/levitation_quiz_spec.rb
@@ -1,6 +1,7 @@
 require_relative "spec_helper"
 require_relative "../levitation_quiz.rb"
 
+
 describe "#levitation_quiz" do 
 	it "puts out the question 'What is the spell that enacts levitation?' until a user types 'Wingardium Leviosa'. Then, it puts out 'You passed the quiz!'" do 
 		allow(self).to receive(:gets).and_return("Wingardium Leviosa")
