81d7af67fdf6569ae4564cd0c55a0f008463b37f
kodyclemens <kody.clemens@gmail.com>
2018-09-28 20:02:04 -0400

    Done

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,21 @@
-
+# Solution without using a break
 def levitation_quiz
-	#your code here
+  spell = ''
+
+  until spell == 'Wingardium Leviosa'
+    puts 'What is the spell that enacts levitation?'
+  	spell = gets.chomp
+  end
+  puts 'You passed the quiz!'
 end
 
+# Solution when using break
 
+def levitation_quiz
+  loop do
+    puts 'What is the spell that enacts levitation?'
+  	spell = gets.chomp
+    break if spell = 'Wingardium Leviosa'
+  end
+  puts 'You passed the quiz!'
+end
